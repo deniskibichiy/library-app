679a2769704c5c9efdc9364745798e060ac7f52b
Denis Kibichiy <deniskibichiy@outlook.com>
2025-09-12 09:55:32 +0300

    loop through the myLibrary array to display the main properties of each book

--- index.js
+++ index.js
@@ -10,6 +10,28 @@ function Book (author, title, pages) {
 
 function createBook (author, title, pages) {
     const newBook = new Book (author, title, pages);
-    newLibrary.push(newBook);
+    myLibrary.push(newBook);
     return newBook;
 }
+
+const powersOfLaw = createBook ("Robert Greene", "The Art of War", 576);
+const atomicHabits = createBook("James Clear", "Atomic Habits", 898);
+function displayBooks () {
+    myLibrary.forEach((item)=> {
+    for (let prop in item) {
+        switch (prop) {
+            case "author":
+                console.log(`Author is ${item[prop]}`);
+                break;
+            case "title":
+                console.log(`Title is ${item[prop]}`);
+                break;
+            case "pages":
+                console.log(`Number of pages is ${item[prop]}`);
+                break;
+         };
+    };
+    })
+};
+
+displayBooks()
