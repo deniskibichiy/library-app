b0252394ea624c99180d59cfd57a73efd7d48e5a
Denis Kibichiy <deniskibichiy@outlook.com>
2025-09-12 21:32:45 +0300

    implement a modal to capture the details of a new book once the button is pressed

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const container = document.querySelector(".container");
-
 const myLibrary = [];
 
 function Book (author, title, pages) {
@@ -18,6 +17,7 @@ function createBook (author, title, pages) {
 
 const powersOfLaw = createBook ("Robert Greene", "The Art of War", 576);
 const atomicHabits = createBook("James Clear", "Atomic Habits", 898);
+const disciplineFreedom = createBook("Jock Willink", "Discipline equals freedom", 98)
 function displayBooks () {
     myLibrary.forEach((item)=> {
         const bookDiv = document.createElement("div");
@@ -26,28 +26,49 @@ function displayBooks () {
         const bookAuthor = document.createElement("p");
         const bookPages = document.createElement("p");
         const readStatus = document.createElement("input");
+        let readNode = document.createElement("p");
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete Book"
+        readNode.textContent = `Read Status: `
         readStatus.setAttribute("type", "checkbox");
         for (let prop in item) {
         switch (prop) {
             case "author":
-                bookAuthor.textContent = `Author is ${item[prop]}`;
+                bookAuthor.textContent = `Author: ${item[prop]}`;
                 bookDiv.appendChild(bookAuthor);
                 break;
             case "title":
-                bookTitle.textContent = `Title is ${item[prop]}`;
+                bookTitle.textContent = `Title: ${item[prop]}`;
                 bookDiv.appendChild(bookTitle)
                 break;
             case "pages":
-                bookPages.textContent = `Number of pages is ${item[prop]}`;
+                bookPages.textContent = `Number of pages: ${item[prop]}`;
                 bookDiv.appendChild(bookPages);
                 break;
             case "isRead":
-                console.log(`The Books's read status is ${item[prop]}`);
-                break;
-         };
+                if(item.isRead) {
+                    readStatus.setAttribute("checked", "");
+                    }
+                    readNode.appendChild(readStatus)
+                    bookDiv.appendChild(readNode);
+                };
     }
+    bookDiv.appendChild(deleteButton)
     container.appendChild(bookDiv);
     })
 };
-
+disciplineFreedom.isRead = true;
+//myLibrary.splice(2)
 displayBooks()
+const btn = document.querySelector(".btn");
+let openDialog = document.querySelector("dialog");
+const closeButton = document.querySelector("#submit");
+btn.addEventListener("click",() => {
+    openDialog.showModal();
+
+})
+
+closeButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    openDialog.close();
+})
